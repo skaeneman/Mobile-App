f8b78c8d2fa2e32fbb8e418c1b8b5747a11a81ec
unknown <unknown@example.com>
2019-04-15 00:51:45 -0400

    Add tabs to parking-spaces homepage

--- ionic-angular-app/src/app/app-routing.module.ts
+++ ionic-angular-app/src/app/app-routing.module.ts
@@ -5,12 +5,6 @@ const routes: Routes = [
   { path: '', redirectTo: 'parking-spaces', pathMatch: 'full' },
   { path: 'authentication', loadChildren: './authentication/authentication.module#AuthenticationPageModule' },
   { path: 'parking-spaces', loadChildren: './parking-spaces/parking-spaces.module#ParkingSpacesPageModule' },
-  { path: 'find', loadChildren: './parking-spaces/find/find.module#FindPageModule' },
-  { path: 'bids', loadChildren: './parking-spaces/bids/bids.module#BidsPageModule' },
-  { path: 'new-bid', loadChildren: './parking-spaces/bids/new-bid/new-bid.module#NewBidPageModule' },
-  { path: 'edit-bid', loadChildren: './parking-spaces/bids/edit-bid/edit-bid.module#EditBidPageModule' },
-  { path: 'space-info', loadChildren: './parking-spaces/find/space-info/space-info.module#SpaceInfoPageModule' },
-  { path: 'space-reservations', loadChildren: './parking-spaces/bids/space-reservations/space-reservations.module#SpaceReservationsPageModule' },
   { path: 'reservations', loadChildren: './reservations/reservations.module#ReservationsPageModule' }
 ];
 
