dff5404ec77043b08c3253b22c009abc241d75c3
unknown <unknown@example.com>
2019-04-15 21:12:09 -0400

    Add tabs and routingq

---
+++ ionic-angular-app/src/app/parking-spaces/parking-spaces-routing.module.ts
@@ -0,0 +1,66 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ParkingSpacesPage } from './parking-spaces.page';
+
+const routes: Routes = [
+  {
+      path: 'tabs',
+      component: ParkingSpacesPage,
+      children: [
+          {
+              path: 'find', children: [
+              {
+                  path: '',
+                  loadChildren: './find/find.module#FindPageModule'
+              },
+              {
+                  path: ':parkingSpaceId',
+                  loadChildren: './find/space-info/space-info.module/#SpaceInfoPageModule'
+              }
+          ]
+        },
+        {
+            path: 'bids',
+            children: [
+                {
+                    path: '',
+                    loadChildren: './bids/bids.module#BidsPageModule'
+                 },
+                 {
+                     // new bid route
+                     path: 'new',
+                     loadChildren: './bids/new-bid/new-bid.module#NewBidPageModule'
+                 },
+                 {
+                     // edit bid route
+                     path: 'edit/:parkingSpaceId',
+                     loadChildren: './bids/edit-bid/edit-bid.module#EditBidPageModule'
+                 },
+                 {
+                     // space reservations
+                     path: 'parkingSpaceId',
+                     loadChildren: './bids/space-reservations/space-reservations.module#SpaceReservationsPageModule'
+                 }
+            ]
+        },
+        {
+            // homepage
+            path: '',
+            redirectTo: '/parking-spaces/tabs/find',
+            pathMatch: 'full'
+        }
+      ]
+  },
+  {
+      // homepage
+      path: '',
+      redirectTo: '/parking-spaces/tabs/find',
+      pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ParkingSpacesRoutingModule { }
--- ionic-angular-app/src/app/parking-spaces/parking-spaces.module.ts
+++ ionic-angular-app/src/app/parking-spaces/parking-spaces.module.ts
@@ -1,25 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ParkingSpacesPage } from './parking-spaces.page';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: ParkingSpacesPage
-  }
-];
+import { ParkingSpacesRoutingModule } from './parking-spaces-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    ParkingSpacesRoutingModule
   ],
   declarations: [ParkingSpacesPage]
 })
