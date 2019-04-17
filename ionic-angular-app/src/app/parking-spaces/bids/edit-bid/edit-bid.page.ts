0677e70ad958e7e93f114d5a97dafc5d9f8254ac
unknown <unknown@example.com>
2019-04-16 22:59:22 -0400

    Edit-bid and space-info updates

--- ionic-angular-app/src/app/parking-spaces/bids/edit-bid/edit-bid.page.ts
+++ ionic-angular-app/src/app/parking-spaces/bids/edit-bid/edit-bid.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ParkingSpacesService } from '../../parking-spaces.service';
+import { NavController } from '@ionic/angular';
+import { ParkingSpaces } from '../../parking-spaces.model';
+
 
 @Component({
   selector: 'app-edit-bid',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-bid.page.scss'],
 })
 export class EditBidPage implements OnInit {
+  parkingSpace: ParkingSpaces;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private parkingSpacesService: ParkingSpacesService,
+    private navController: NavController
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      // if there is no parkingSpaceId
+      if (!paramMap.has('parkingSpaceId')) {
+        this.navController.navigateBack('/parking-spaces/tabs/bids');
+        return;
+      }
+      // finds parking spot by id
+      this.parkingSpace = this.parkingSpacesService.findParkingSpace(paramMap.get('parkingSpaceId'));
+    });
   }
 
 }
--- ionic-angular-app/src/app/parking-spaces/find/space-info/space-info.page.ts
+++ ionic-angular-app/src/app/parking-spaces/find/space-info/space-info.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { ParkingSpacesService } from '../../parking-spaces.service';
+import { ParkingSpaces } from '../../parking-spaces.model';
 
 @Component({
   selector: 'app-space-info',
@@ -8,10 +10,25 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./space-info.page.scss'],
 })
 export class SpaceInfoPage implements OnInit {
+  parkingSpace: ParkingSpaces;
 
-  constructor(private router: Router, private navController: NavController) { }
+  constructor(
+    private navController: NavController,
+    private activatedRoute: ActivatedRoute,
+    private parkingSpacesService: ParkingSpacesService
+  ) { }
 
   ngOnInit() {
+    // setup navigation
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      // if there is no parkingSpaceId
+      if (!paramMap.has('parkingSpaceId')) {
+        this.navController.navigateBack('/parking-spaces/tabs/find');
+        return;
+      }
+      // finds parking spot by id
+      this.parkingSpace = this.parkingSpacesService.findParkingSpace(paramMap.get('parkingSpaceId'));
+    });
   }
 
   reserveSpot() {
