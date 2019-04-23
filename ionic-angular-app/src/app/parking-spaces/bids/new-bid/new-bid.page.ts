253b1d98e086c0fbe5862663bd7ef9e67de3e8bb
unknown <unknown@example.com>
2019-04-23 19:51:26 -0400

    Create new-bid logic for form

--- ionic-angular-app/src/app/parking-spaces/bids/new-bid/new-bid.page.ts
+++ ionic-angular-app/src/app/parking-spaces/bids/new-bid/new-bid.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-bid',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-bid.page.scss'],
 })
 export class NewBidPage implements OnInit {
+newForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    // create a new form on page load
+    this.newForm = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(100)]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      from: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      to: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
   }
+
+onCreateBid() {
+  console.log('bid created...');
+}
 
 }
